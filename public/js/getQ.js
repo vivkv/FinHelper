d7adfa58ba1950e78eda1b17d2a7d82203b51a0a
vivkv <vivek.pathak@gmail.com>
2019-06-05 20:27:49 +0530

    added thumbs up and thumbs down along with bug fix of answer repetition on clicks

--- public/js/getQ.js
+++ public/js/getQ.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $("#btn").click(function(event){
-
     $(".input-group.customDiv").css("padding","10px");
     $(".input-group.customDiv").appendTo("#dest");
     $.ajax({
@@ -9,9 +8,14 @@ $(document).ready(function(){
       data: JSON.stringify({"que": $("#question").val()}),
       contentType: "application/json; charset=utf-8",
       success: function(data){
+        $(".bodyarea").empty();
         $.each(data,function(key,val){
           $(".bodyarea").append("<div class=\"answers custom-card card w-100\">"+
-          "<div class=\"card-text\">"+key +" : "+ val+
+            "<div class=\"card-text\">"+key +" : "+ val+
+            "<div class=\"thumbs\">"+
+              "<img class=\"up img-fluid rounded float-right\" src=\"/../img/thumbsDown.png\">"+
+              "<img class=\"up img-fluid rounded float-right\" src=\"/../img/thumbsUp.png\">"+
+            "</img></img></div>"+
           "</div></div>");
         });
       }
